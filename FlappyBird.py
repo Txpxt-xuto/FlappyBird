d3fe3bf0a5c4ab4b86469c4337411d010190a674
Tapat toungsaakul <56795188+Txpxt-xuto@users.noreply.github.com>
2024-03-17 08:54:23 +0700

    Update FlappyBird.py

--- FlappyBird.py
+++ FlappyBird.py
@@ -24,7 +24,7 @@ def pipe_animation():
         else:
             screen.blit(pipe_img, pipe)
         pipe.centerx -=3
-        if pip.right < 0:
+        if pipe.right < 0:
             pipes.remove(pipe)
         if bird_rect.colliderect(pipe):
             game_over = True
@@ -49,22 +49,22 @@ def draw_score(game_state):
         score_rect = score_text.get_rect(center=(width // 2,66))
         screen.blit(score_text,score_rect)
 
-        high_sacore_text = score_font.render(f"Score:{high_score}",True,(255,255,255))
+        high_score_text = score_font.render(f"Score:{high_score}",True,(255,255,255))
         high_score_rect = high_score_text.get_rect(center=(width // 2,506))
-        screen.blit(high_sacore_text,high_score_rect)
+        screen.blit(high_score_text,high_score_rect)
 
 width, height = 350, 622
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("FlappyBird")
 
-back_img = pygame.image.load("")
-floor_img = pygame.image.load("")
+back_img = pygame.image.load("bg_flappybird.jpg")
+floor_img = pygame.image.load("floor_img.jpg")
 floor_x = 0
 
-bird_up = pygame.image.load("")
-bird_down = pygame.image.load("")
-bird_mid = pygame.image.load("")
+bird_up = pygame.image.load("bird_img_mid.jpg")
+bird_down = pygame.image.load("bird_img_mid.jpg")
+bird_mid = pygame.image.load("bird_img_mid.jpg")
 birds = [bird_down,bird_mid,bird_up]
 bird_index = 0
 bird_flap = pygame.USEREVENT
@@ -74,9 +74,81 @@ bird_rect = bird_img.get_rect(center=(67,622 // 2))
 bird_movement = 0
 gravity = 0.17
 
-pipe_img = pygame.image.load("")
+pipe_img = pygame.image.load("pipe.jpg")
 pipe_height = [400,350,533,490]
 
 pipes = []
 create_pipe = pygame.USEREVENT + 1
 pygame.time.set_timer(create_pipe, 1200)
+
+game_over = False
+over_img = pygame.image.load("gameover.jpg").convert_alpha()
+over_rect = over_img.get_rect(center=(width // 2, height // 2))
+
+score = 0
+high_score = 0
+score_time = True
+score_font = pygame.font.Font("Tahoma",27)
+
+running = True
+while running:
+    clock.tick(120)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            sys.exit()
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE and not game_over:
+                bird_movement = 0
+                bird_movement = -7
+
+            if event.key == pygame.K_SPACE and game_over:
+                game_over = False
+                pipes = []
+                bird_movement = 0
+                bird_rect = bird_img.get_rect(center=(67,622 // 2))
+                score_time = True
+                score = 0
+        if event.type == bird_flap:
+            bird_index +=1
+            
+            if bird_index > 2:
+                bird_index = 0
+            
+            bird_img = birds[bird_index]
+            bird_rect = bird_up.get_rect(center = bird_rect.center)
+
+        if event.type == create_pipe:
+            pipes.extend(create_pipes())
+
+    screen.blit(floor_img, (floor_x, 550))
+    screen.blit(back_img, (0, 0))
+
+    if not game_over:
+        bird_movement += gravity
+        bird_rect.centery += bird_movement
+        rotated_bird = pygame.transform.rotozoom(bird_img,bird_movement*-6,1)
+
+        if bird_rect.top < 5 or bird_rect.bottom >=550:
+            game_over = True
+        
+        screen.blit(rotated_bird, bird_rect)
+        pipe_animation()
+        score_update()
+        draw_score("game_on")
+
+    elif game_over:
+        screen.blit(over_img, over_rect)
+        draw_score("game_over")
+
+    floor_x -=1
+    if floor_x < -448:
+        floor_x = 0
+
+    draw_floor()
+
+    pygame.display.update()
+
+pygame.quit()
+sys.exit()
